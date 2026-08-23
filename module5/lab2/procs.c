31b0c58d96e90c4302c7ac355084f7c08e491772
kastomara <yandex.rout.ru@gmail.com>
2026-08-23 16:29:45 +0700

    [m5] new vercion of procs module

--- module5/lab2/procs.c
+++ module5/lab2/procs.c
@@ -5,37 +5,47 @@
 #include <linux/sched.h>
 #include <linux/slab.h>
 
-#define COUNT_SIZE 10;
-#define FILE_NAME "proc";
+#define COUNT_SIZE 50
+#define FILE_NAME "proc"
 
 static int len,temp;
 static char *msg;
  
-static ssize_t read_proc(struct file *filp, char *buf, size_t count, loff_t *offp ) {
+static ssize_t read_proc(struct file *filp, char *buf, size_t count, loff_t *offp) {
     if(count > temp) {
         count = temp;
     }
     temp = temp - count;
-    copy_to_user(buf, msg, count);
+    if(copy_to_user(buf, msg, count)) {
+		return -EFAULT;
+	}
     if(count == 0)
         temp = len;
     return count;
 }
  
 static ssize_t write_proc(struct file *filp, const char *buf, size_t count, loff_t *offp) {
-    copy_from_user(msg, buf, count);
+	memset(msg, 0, COUNT_SIZE);
+    if (count > COUNT_SIZE) {
+		count = COUNT_SIZE;
+	}
+    if(copy_from_user(msg, buf, count)) {
+		return -EFAULT;
+	}
+    msg[count] = '\0';
     len = count;
     temp = len;
+    *offp = 0;
     return count;
 }
  
 static const struct proc_ops proc_fops = {
-    proc_read: read_proc,
-    proc_write: write_proc,
+    .proc_read = read_proc,
+    .proc_write = write_proc,
 };
  
 static void create_new_proc_entry(void) {
-    proc_create(FILE_NAME, 0, NULL, &proc_fops);
+    proc_create(FILE_NAME, 0666, NULL, &proc_fops);
     msg = kmalloc(COUNT_SIZE * sizeof(char), GFP_KERNEL);
 }
  
@@ -45,12 +55,12 @@ static int __init proc_init (void) {
 }
  
 static void __exit proc_cleanup(void) {
-    remove_proc_entry("hello", NULL);
+    remove_proc_entry(FILE_NAME, NULL);
     kfree(msg);
 }
 
 module_init(proc_init);
-module_exit(proc_exit);
+module_exit(proc_cleanup);
 
 MODULE_LICENSE("Увы");
 MODULE_AUTHOR("Астанин Артём");
