f6103f6bf6d915a8e61f2319777e737cc9ac17f8
kastomara <yandex.rout.ru@gmail.com>
2026-08-25 15:57:47 +0700

    [m5] working version

--- module5/lab2/procs.c
+++ module5/lab2/procs.c
@@ -8,7 +8,7 @@
 #define COUNT_SIZE 50
 #define FILE_NAME "proc"
 
-static int len,temp;
+static int len, temp;
 static char *msg;
  
 static ssize_t read_proc(struct file *filp, char *buf, size_t count, loff_t *offp) {
@@ -17,21 +17,21 @@ static ssize_t read_proc(struct file *filp, char *buf, size_t count, loff_t *off
     }
     temp = temp - count;
     if(copy_to_user(buf, msg, count)) {
-		return -EFAULT;
-	}
+        return -EFAULT;
+    }
     if(count == 0)
         temp = len;
     return count;
 }
  
 static ssize_t write_proc(struct file *filp, const char *buf, size_t count, loff_t *offp) {
-	memset(msg, 0, COUNT_SIZE);
+    memset(msg, 0, COUNT_SIZE);
     if (count > COUNT_SIZE) {
-		count = COUNT_SIZE;
-	}
+        count = COUNT_SIZE;
+    }
     if(copy_from_user(msg, buf, count)) {
-		return -EFAULT;
-	}
+        return -EFAULT;
+    }
     msg[count] = '\0';
     len = count;
     temp = len;
@@ -39,23 +39,24 @@ static ssize_t write_proc(struct file *filp, const char *buf, size_t count, loff
     return count;
 }
  
-static const struct proc_fops = {
+static const struct proc_ops proc_fops = {
     .proc_read = read_proc,
     .proc_write = write_proc,
 };
  
-static void create_new_proc_entry(void) {
-    proc_create(FILE_NAME, 0660, NULL, &proc_fops);
+static int create_new_proc_entry(void) {
     msg = kmalloc(COUNT_SIZE * sizeof(char), GFP_KERNEL);
-//    if (msg = NULL) {
-//        printk("Не удалось выделить запрашиваемый объем памяти");
-//        break;
-//    }
+    
+    if (msg == NULL) {
+        printk(KERN_ERR "Не удалось выделить запрашиваемый объем памяти\n");
+        return -ENOMEM; 
+    }
+    proc_create(FILE_NAME, 0660, NULL, &proc_fops);
+    return 0;
 }
  
-static int __init proc_init (void) {
-    create_new_proc_entry();
-    return 0;
+static int __init proc_init(void) {
+    return create_new_proc_entry();
 }
  
 static void __exit proc_cleanup(void) {
@@ -66,6 +67,6 @@ static void __exit proc_cleanup(void) {
 module_init(proc_init);
 module_exit(proc_cleanup);
 
-MODULE_LICENSE("Увы");
+MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Астанин Артём");
 MODULE_DESCRIPTION("Сделал модуль ядра для первого задания по новому модулю. Модуль модуль модуль");
